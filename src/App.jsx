669f395f689f3bfe41773e411811678149224136
kiraa123 <kiranprakash970@gmail.com>
2024-03-25 20:36:42 +0530

    production list

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Slider from './components/Slider'
+import Production from './components/Production'
 
 function App() {
 
   return (
     <div className=''>
      <Header/>
+     <Slider/>
+     <Production/>
 
     </div>
    
