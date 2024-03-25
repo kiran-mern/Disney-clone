aa4059449d8fc951cd6de9da429ad91423d4435c
kiraa123 <kiranprakash970@gmail.com>
2024-03-25 19:42:26 +0530

    banner adding

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,22 +1,43 @@
 import { useRef } from 'react'
+import { movieList } from '../constants/MovieList'
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 const screenWidth = window.innerWidth
 
 const Slider = () => {
     const elementRef = useRef();
-    const elemetRight = (element) => {
+    const sliderRight = (element) => {
         element.scrollLeft += screenWidth - 110;
     }
-    const elemetLeft = (element) => {
+    const sliderLeft = (element) => {
         element.scrollLeft += screenWidth - 110;
 
     }
     return (
         <div className=''>
             <HiChevronLeft
-
-            
+                className='hidden md:block text-white text-[30px] absolute
+                            mx 8 mt-[225px] cursor-pointer'
+                onClick={() => sliderLeft(elementRef.current)}
             />
+            <HiChevronRight
+                className="hidden md:block text-white text-[30px] absolute
+        mx-8 mt-[225px] cursor-pointer right-0"
+                onClick={() => sliderRight(elementRef.current)}
+            />
+            <div
+                className="flex overflow-x-auto w-full px-16 py-4
+                            scrollbar-none scroll-smooth"
+                ref={elementRef}
+            >
+                {movieList.map((item) => (
+                    <img
+                        src={item}
+                        alt="movie"
+                        className="min-w-full md:h-auto lg:h-[500px] object-cover
+        object-left-top mr-5 rounded-md hover:border-[4px] border-gray-700 transition-all duration-100 ease-in"
+                    />
+                ))}
+            </div>
 
 
         </div>
---
+++ src/constants/MovieList.jsx
@@ -0,0 +1,25 @@
+export const movieList = [
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/3050/1710437163050-i",
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/8730/1707996958730-i",
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/5549/1665549-i-67812177cf3d",
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/5212/1545212-i-163a4ee38593",
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/833/1650833-i-0fe6a37d72ce",
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/1791/1611791-i-3b22c4f693bd",
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/7013/1657013-i-0433f19bb907",
+    "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/4963/1474963-i-c484075786f0",
+  ];
+  
+  export const movies = [ 
+    "https://i.ytimg.com/vi_webp/YklvWrS7VzA/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/3Fk9IFfYkEs/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/vO2N0LGvhWc/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/GZQlT5RgDVI/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/-zPXY1ER-1w/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/DbGWQBNrx6c/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/NJZTCJKGyx4/movieposter.webp",
+    "https://i.ytimg.com/vi_webp/nHY0sjIK94k/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/CJbL3Ve3JGQ/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/fXLLVIpMVsA/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/XoJKQqhCDEQ/movieposter_en.webp",
+    "https://i.ytimg.com/vi_webp/Df5XvoZQ5as/movieposter_en.webp"
+  ];
